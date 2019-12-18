08404d65388869d866c85a8e09b2efc4dbe52ce3
Raphael Attie <attie.raphael@gmail.com>
2019-12-18 12:47:56 -0500

    adding housekeeping script

---
+++ fits_check_rename.py
@@ -0,0 +1,46 @@
+import os, glob
+from pathlib import Path
+import fitsio
+from multiprocessing import Pool
+import time
+
+def recompress(fits_file):
+    spikes = fitsio.read(fits_file)
+    new_fits_f = fits_file.parents[0] / str(fits_file.stem + '_c.fits')
+    print(new_fits_f)
+    fitsio.write(new_fits_f, spikes, compress='rice', clobber=True)
+    os.remove(fits_file)
+
+
+def check_compression(fits_file):
+    fits = fitsio.FITS(fits_file)
+    if len(fits) == 1:
+        print(str(fits_file))
+
+def check_compression_rename_fits(fits_file):
+    fits = fitsio.FITS(fits_file)
+    if len(fits) == 1:
+        print(str(fits_file))
+    fits.close()
+    new_fits_f = fits_file.parents[0] / str(fits_file.name).replace(':', '_')
+    os.rename(fits_file, new_fits_f)
+
+
+datadir = os.path.join(os.environ['DATA'], 'michael/spikes_project/AIA_spikes/temp/13')
+print('listing files...')
+flist = sorted(Path(datadir).rglob('*.fits'))
+
+print('Starting parallel jobs...')
+with Pool(processes=4) as p:
+    p.map(check_compression_rename_fits, flist)
+
+# # Get wall clock time:
+# tstart = time.time()
+# with Pool(processes=4) as p:
+#     p.map(recompress, flist)
+#
+# telapsed = time.time() - tstart
+#
+# print('elapsed time (sec) : ', telapsed)
+
+
