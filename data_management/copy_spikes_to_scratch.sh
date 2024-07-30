d9d4525b1e9391fb9fbc32a065dc2d4898bce701
Raphael Attie <attie.raphael@gmail.com>
2024-07-30 13:45:09 -0400

    changed hopper rsync of spikes to simple timed copy commands

---
+++ data_management/copy_spikes_to_scratch.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+time cp /groups/SPIKES/downloads/spikes_2011*.tar /scratch/rattie/spikes/
+time cp /groups/SPIKES/downloads/spikes_2012*.tar /scratch/rattie/spikes/
+time cp /groups/SPIKES/downloads/spikes_2013*.tar /scratch/rattie/spikes/
+time cp /groups/SPIKES/downloads/spikes_2014*.tar /scratch/rattie/spikes/
+time cp /groups/SPIKES/downloads/spikes_2015*.tar /scratch/rattie/spikes/
+time cp /groups/SPIKES/downloads/spikes_2016*.tar /scratch/rattie/spikes/
+time cp /groups/SPIKES/downloads/spikes_2017*.tar /scratch/rattie/spikes/
+time cp /groups/SPIKES/downloads/spikes_2018*.tar /scratch/rattie/spikes/
+time cp /groups/SPIKES/downloads/spikes_2019*.tar /scratch/rattie/spikes/
+time cp /groups/SPIKES/downloads/spikes_2020*.tar /scratch/rattie/spikes/
+time cp /groups/SPIKES/downloads/spikes_2021*.tar /scratch/rattie/spikes/
+time cp /groups/SPIKES/downloads/spikes_2022*.tar /scratch/rattie/spikes/
